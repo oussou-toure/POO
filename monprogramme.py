86f9057f8a3386caf208be21e688dce73f1baee7
Ousmane Touré <oussoutoure@outlook.com>
2023-11-26 00:39:58 +0000

    Création de la classe Person

---
+++ monprogramme.py
@@ -0,0 +1,9 @@
+#Create my class
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age =age
+        
+#Attributes and methods
+    def greet(self):
+        print("Hello, my name is", self.name)
