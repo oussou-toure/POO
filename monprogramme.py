d6b35e98a29efaeb9337a490da5c4803baca17f9
Ousmane Touré <oussoutoure@outlook.com>
2023-11-26 00:45:55 +0000

    Ajout de la classe Student

--- monprogramme.py
+++ monprogramme.py
@@ -7,3 +7,12 @@ class Person:
 #Attributes and methods
     def greet(self):
         print("Hello, my name is", self.name)
+
+#Inheritance
+class Student(Person):
+    def __init__(self, name, age, major):
+        super().__init__(name, age)
+        self.major = major
+    
+    def study(self):
+        print(self.name, "is studying", self.major)
